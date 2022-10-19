ab3b8e4526807543aabe580a2f9f0d5dbd50db2b
Towsif Hossain <towsifhossain@Towsifs-MeowcBook-Pro.local>
2022-10-19 23:47:22 +0600

    python script output piping successful

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const cors = require("cors");
+const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
+const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
 
-app.listen(5000, () => {
-  console.log("Backend server running...");
+app.get("/run-pyscript", (req, res) => {
+  var str = "meow";
+  let output;
+
+  const python = spawn("python3", ["pyscripts/script.py", str]);
+  python.stdout.on("data", (data) => {
+    console.log("Piping data from python script...");
+    console.log(data.toString());
+    output = data.toString();
+  });
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    res.json({ output: output });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server running @${PORT}`);
 });
