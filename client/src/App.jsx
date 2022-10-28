ec4466726ebd119e963f59e45bf96e7a435ae2c2
Towsif Hossain <towsifhossain5120@gmail.com>
2022-10-29 03:43:04 +0600

    UI backbone ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 const App = () => {
-  return <div>Hello world!</div>;
+  const [result, setResult] = useState("");
+  const handleClick = () => {
+    setResult("Clicked");
+  };
+  return (
+    <>
+      <Container>
+        <Box
+          component="form"
+          autoComplete="off"
+          noValidate
+          alignItems="center"
+          justifyContent="center"
+          marginX={20}
+        >
+          <Typography variant="h4">Toxic Comment Detector</Typography>
+          <TextField
+            id="comment"
+            fullWidth
+            placeholder="Write something here..."
+            rows={3}
+            multiline
+          />
+          <Button variant="contained" onClick={handleClick}>
+            Check
+          </Button>
+          {result !== "" && <Typography>{result}</Typography>}
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default App;
